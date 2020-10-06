81c2514b4e4611017533516d044f21994867ef53
Karl Dinwiddie <karldinwiddie@gmail.com>
2020-10-06 05:55:50 -0700

    .

--- src/main/java/com/panopset/flywheel/CommandList.java
+++ src/main/java/com/panopset/flywheel/CommandList.java
@@ -1,6 +1,5 @@
 package com.panopset.flywheel;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.List;
--- src/test/java/com/panopset/flywheel/BracketTest.java
+++ src/test/java/com/panopset/flywheel/BracketTest.java
@@ -1,5 +1,7 @@
 package com.panopset.flywheel;
 
+import static com.panopset.compat.Stringop.BAR;
+import static com.panopset.compat.Stringop.FOO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
@@ -7,20 +9,8 @@ import com.panopset.compat.Logop;
 
 /**
  * Tests the Flywheel template variable resolution.
- *
- * @author Karl Dinwiddie
  */
-public final class BracketTest {
-
-  /**
-   * <b>foo</b>.
-   */
-  static final String FOO = "foo";
-
-  /**
-   * <b>bar</b>.
-   */
-  static final String BAR = "bar";
+final class BracketTest {
 
   /**
    * <b>$&#123;1} ${foo}</b>.
